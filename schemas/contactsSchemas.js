9cc393340f29e247ad83068ab2ed14040afcc85c
salandr <ansaliy200013@gmail.com>
2024-05-04 13:45:07 +0100

    task finished

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,24 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .regex(/^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+(?!(\.ru|\.su))\.[a-zA-Z]{2,}$/)
+    .message("Enter email of an existing country")
+    .required(),
+  phone: Joi.string()
+    .regex(/^[0-9]{10}$/)
+    .message("Phone number must have 10 digits.")
+    .required(),
+});
 
 export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  email: Joi.string()
+    .regex(/^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+(?!(\.ru|\.su))\.[a-zA-Z]{2,}$/)
+    .message("Enter email of an existing country"),
 
-})
+  phone: Joi.string()
+    .regex(/^[0-9]{10}$/)
+    .message("Phone number must have 10 digits."),
+});
